7742d3bb9bf18960ae12286c5775ddead3f1d71a
gabrielamb2k <gambomfim9@gmail.com>
2025-04-15 16:12:06 -0300

    adiciona componentes e define algumas rotas

---
+++ src/app/pages/not-logged/home/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../componentes/not-logged/header/header.component';
+import { SignInComponent } from '../../../componentes/not-logged/sign-in/sign-in.component';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [HeaderComponent, SignInComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
